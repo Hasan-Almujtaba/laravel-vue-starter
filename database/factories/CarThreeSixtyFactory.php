cf1022b0d2ee49ee2e1004942572e1a6a34a985a
rogersovich <dimasroger89@gmail.com>
2020-08-31 19:39:54 +0700

    add crud car three sixty

---
+++ database/factories/CarThreeSixtyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\CarThreeSixty;
+use Faker\Generator as Faker;
+
+$factory->define(CarThreeSixty::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- database/migrations/2020_08_31_145626_create_car_three_sixties_table.php
+++ database/migrations/2020_08_31_145626_create_car_three_sixties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarSpecificationsTable extends Migration
+class CreateCarThreeSixtiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateCarSpecificationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('car_specifications', function (Blueprint $table) {
+        Schema::create('car_three_sixties', function (Blueprint $table) {
             $table->id();
             $table->foreignId('car_id')->constrained()->onDelete('cascade');
-            $table->string('desc');
-            $table->string('engine');
-            $table->string('rate_output');
-            $table->string('rate_tongue');
+            $table->string('path');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateCarSpecificationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('car_specifications');
+        Schema::dropIfExists('car_three_sixties');
     }
 }
