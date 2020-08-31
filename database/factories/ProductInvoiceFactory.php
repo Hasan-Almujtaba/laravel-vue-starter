58701f8bd27a963520a28318577281801b573ad8
rogersovich <dimasroger89@gmail.com>
2020-08-31 21:13:11 +0700

    add new product invoice

---
+++ database/factories/ProductInvoiceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ProductInvoice;
+use Faker\Generator as Faker;
+
+$factory->define(ProductInvoice::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
